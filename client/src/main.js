9ebc411523c98bb80546869c4add8ca2d6906fd0
lathief <shadowmaster241099@yahoo.co.id>
2023-05-08 18:10:28 +0700

    fix bug dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -1,20 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import "bootstrap/dist/css/bootstrap.min.css"
-import './assets/main.css'
-/* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
 import { faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 
-// /* add icons to the library */
+import "bootstrap/dist/css/bootstrap.min.css"
+
 library.add(faPlus, faEdit, faTrash)
 
-// /* add font awesome icon component */    
-createApp(App).use(VueAxios, axios).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+const app = createApp(App)
+
+app.config.productionTip = false
+app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
