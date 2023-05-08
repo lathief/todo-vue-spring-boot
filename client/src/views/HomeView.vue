c5f14c36b5fb377a0b331da3e865759abbc32c8b
lathief <shadowmaster241099@yahoo.co.id>
2023-05-08 18:11:14 +0700

    make login and register view

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container d-flex flex-column min-vh-100">
+    <header
+      class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
+    >
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a
+          href="/"
+          class="d-inline-flex link-body-emphasis text-decoration-none"
+        >
+          <svg
+            class="bi"
+            width="40"
+            height="32"
+            role="img"
+            aria-label="Bootstrap"
+          >
+            <use xlink:href="#bootstrap"></use>
+          </svg>
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <h1 style="text-align: center; font-size: 32px">Todo List</h1>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">
+          Login
+        </button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+      </div>
+    </header>
+    <div class="todoListContainer">
+      <addItemForm />
+      <listView />
+    </div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import addItemForm from "../components/addItemForm.vue";
+import listView from "../components/listView.vue";
+import Footer from "../components/footer.vue";
+export default {
+name:'HomeView',
+  components: {
+    addItemForm,
+    listView,
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+.todoListContainer {
+  margin: auto;
+  margin-top: 25px;
+}
+</style>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container d-flex flex-column min-vh-100">
+    <header class="d-flex justify-content-center py-3 border-bottom">
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link">
+            <h1 style="text-align: center; font-size: 32px">Todo List</h1>
+        </a></li>
+      </ul>
+      
+    </header>
+    <div class="form-signin m-auto mt-5">
+      <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="form-floating">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput"
+            placeholder="Username"
+          />
+          <label for="floatingInput">Username</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  padding: 70px;
+  padding-bottom: 40px;
+}
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
