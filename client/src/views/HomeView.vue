750dfa4e93fe18b5e5f9242094bc197696398f09
lathief <shadowmaster241099@yahoo.co.id>
2023-05-09 16:09:57 +0700

    change style header, add router-link auth

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="container d-flex flex-column min-vh-100">
-    <header
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="/" class="nav-link px-2 link-active" style="font-size: 32px;">Todo List</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <RouterLink to="/login" class="btn btn-outline-primary me-2">Sign in</RouterLink>
+        <RouterLink to="/register" class="btn btn-primary">Sign up</RouterLink>
+      </div>
+    </header>
+    <!-- <header
       class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom"
     >
       <div class="col-md-3 mb-2 mb-md-0">
@@ -25,12 +39,24 @@
       </ul>
 
       <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">
-          Login
-        </button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
+        <RouterLink to="/login" class="btn btn-outline-primary me-2">Sign in</RouterLink>
+        <RouterLink to="/register" class="btn btn-primary">Sign up</RouterLink>
       </div>
-    </header>
+    </header> -->
+    <!-- <header class="d-flex justify-content-center py-3 border-bottom">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <a href="/" class="nav-link">
+            <h1 style="text-align: center; font-size: 32px">Todo List</h1>
+          </a>
+        </li>
+      </ul>
+      <ul class="nav text-end">
+        <li class="nav-item"><a href="/" class="nav-link">
+            <h1 style="text-align: center; font-size: 32px">Todo List</h1>
+        </a></li>
+      </ul>
+    </header> -->
     <div class="todoListContainer">
       <addItemForm />
       <listView />
