be7f110e5d24a52d24b828a3c178b87c6e58edb4
lathief <shadowmaster241099@yahoo.co.id>
2023-05-09 16:10:09 +0700

    change style header

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,32 +1,19 @@
 <template>
   <div class="container d-flex flex-column min-vh-100">
-    <header class="d-flex justify-content-center py-3 border-bottom">
-      <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link">
-            <h1 style="text-align: center; font-size: 32px">Todo List</h1>
-        </a></li>
+    <header class="d-flex flex-wrap align-items-center justify-content-center py-3 mb-4 border-bottom">
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="/" class="nav-link px-2 link-active" style="font-size: 32px;">Todo List</a></li>
       </ul>
-      
     </header>
     <div class="form-signin m-auto mt-5">
       <form>
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
         <div class="form-floating">
-          <input
-            type="text"
-            class="form-control"
-            id="floatingInput"
-            placeholder="Username"
-          />
+          <input type="text" class="form-control" id="floatingInput" placeholder="Username" />
           <label for="floatingInput">Username</label>
         </div>
         <div class="form-floating">
-          <input
-            type="password"
-            class="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
           <label for="floatingPassword">Password</label>
         </div>
 
@@ -35,20 +22,20 @@
         </button>
       </form>
     </div>
+    <Footer></Footer>
   </div>
 </template>
 <script>
+import Footer from "../components/footer.vue";
 export default {
   name: "LoginView",
+  components: {
+    Footer,
+  }
 };
 </script>
 <style scoped>
-.container {
-  display: flex;
-  align-items: center;
-  padding: 70px;
-  padding-bottom: 40px;
-}
+
 .form-signin {
   max-width: 330px;
   padding: 15px;
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,82 +1,59 @@
 <template>
-    <div class="container d-flex flex-column min-vh-100">
-      <header class="d-flex justify-content-center py-3 border-bottom">
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a href="#" class="nav-link">
-              <h1 style="text-align: center; font-size: 32px">Todo List</h1>
-          </a></li>
-        </ul>
-        
-      </header>
-      <div class="form-signin m-auto mt-5">
-        <form>
-          <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
-          <div class="form-floating">
-            <input
-              type="text"
-              class="form-control"
-              id="floatingInput"
-              placeholder="Username"
-            />
-            <label for="floatingInput">Username</label>
-          </div>
-          <div class="form-floating">
-            <input
-              type="email"
-              class="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div class="form-floating">
-            <input
-              type="password"
-              class="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label for="floatingPassword">Password</label>
-          </div>
-  
-          <button class="w-100 btn btn-lg btn-primary" type="submit">
-            Sign up
-          </button>
-        </form>
-      </div>
+  <div class="container d-flex flex-column min-vh-100">
+    <header class="d-flex flex-wrap align-items-center justify-content-center py-3 mb-4 border-bottom">
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="/" class="nav-link px-2 link-active" style="font-size: 32px;">Todo List</a></li>
+      </ul>
+    </header>
+    <div class="form-signin m-auto mt-5">
+      <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="form-floating">
+          <input type="text" class="form-control" id="floatingInput" placeholder="Username" />
+          <label for="floatingInput">Username</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form>
     </div>
-  </template>
-  <script>
-  export default {
-    name: "LoginView",
-  };
-  </script>
-  <style scoped>
-  .container {
-    display: flex;
-    align-items: center;
-    padding: 70px;
-    padding-bottom: 40px;
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Footer from "../components/footer.vue";
+export default {
+  name: "RegisterView",
+  components: {
+    Footer,
   }
-  .form-signin {
-    max-width: 330px;
-    padding: 15px;
-  }
-  
-  .form-signin .form-floating:focus-within {
-    z-index: 2;
-  }
-  
-  .form-signin input[type="email"] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-  }
-  
-  .form-signin input[type="password"] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-  }
-  </style>
-  
+};
+</script>
+<style scoped>
+
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
